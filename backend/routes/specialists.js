f6a13bee7a0b4666904ae4db63c91a2e8b63fe9d
EcksV <domcejusk3@gmail.com>
2023-12-13 00:56:07 +0200

    fixed admin token

--- backend/routes/specialists.js
+++ backend/routes/specialists.js
@@ -10,22 +10,30 @@ const {
 
 const specialistRouter = express.Router();
 
-const checkAdmin = (req, res, next) => {
+const checkAdmin = async (req, res, next) => {
     const token = req.cookies.token;
     if (!token) return res.status(401).json({ message: 'Token not provided' });
-    
-    jwt.verify(token, process.env.Secret, (err, decoded) => {
-        if (err || !decoded || !decoded.isAdmin) 
-            return res.status(401).json({ message: 'Invalid or unauthorized token' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.Secret);
+        if (!decoded || !decoded.username) 
+            return res.status(401).json({ message: 'Invalid token' });
+        
+        const user = await AccountModel.findOne({ username: decoded.username });
+        if (!user || !user.admin) 
+            return res.status(401).json({ message: 'Unauthorized user' });
         
         next();
-    });
+    } catch (error) {
+        return res.status(401).json({ message: "Server error occurred" });
+    }
 };
+
 
 specialistRouter.get('/', getAllSpecialists);
 specialistRouter.get('/:id', getSpecialistById);
 specialistRouter.post('/', checkAdmin, addSpecialist);
-specialistRouter.put('/:id', checkAdmin,  updateSpecialist);
+specialistRouter.put('/:id', checkAdmin, updateSpecialist);
 specialistRouter.delete('/:id', checkAdmin, deleteSpecialist);
 
 module.exports = specialistRouter;
