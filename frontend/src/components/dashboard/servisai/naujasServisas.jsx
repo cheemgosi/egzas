ce43dfc9646fafcc35a20eddad17de6e23d3a618
Sliver <matas.jasevicius@gmail.com>
2023-12-12 23:41:44 +0200

    miau

--- frontend/src/components/dashboard/servisai/naujasServisas.jsx
+++ frontend/src/components/dashboard/servisai/naujasServisas.jsx
@@ -5,7 +5,21 @@ const naujasServisas = () => {
     return (
         <div>
             <DashNavbar />
-            Naujas
+            <div className='container-xl bg-secondary'>
+            <form action="/submit_servisas" method="post">
+                <div>
+                  <label for="pavadinimas">Pavadinimas:</label>
+                  <input type="text" id="pavadinimas" name="pavadinimas" required/>
+                </div>
+                <div>
+                  <label for="miestas">Miestas:</label>
+                  <input type="text" id="miestas" name="miestas" required/>
+                </div>
+                <div>
+                  <input type="submit" value="Submit"/>
+                </div>
+              </form>
+            </div>
         </div>
     );
 };
